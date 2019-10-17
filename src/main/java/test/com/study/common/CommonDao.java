9f8c955173e9b84c21b3f90ae0e0f51e8fa704a6
DELL <DELL@JinKyu>
2019-10-17 20:53:19 +0900

    mapper location 설정, 샘플 패키지 생성, CommonDao 생성.

--- src/main/java/test/com/study/common/CommonDao.java
+++ src/main/java/test/com/study/common/CommonDao.java
@@ -1,13 +1,19 @@
 package test.com.study.common;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class CommonDao {
+public class CommonDao extends SqlSessionDaoSupport {
 
-	@Autowired
-	protected SqlSessionTemplate sqlSession;
+	 @Autowired
+	 public SqlSessionTemplate sqlSession;
 	 
-
+	 public String nameSpace;
+	 
+	 public void SetNameSpace(String nameSpace) {
+		 this.nameSpace = nameSpace;
+	 }
+	
 	
 }
